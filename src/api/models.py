b6495c00e5870641627f8f8a829cefd6523276f9
JuanRR17 <juanrepeto@gmail.com>
2022-10-04 09:13:28 +0000

    e-commerce like db

--- src/api/models.py
+++ src/api/models.py
@@ -27,15 +27,43 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
+class Type(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Type {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+class Unit(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    unit = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Unit {self.unit}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "unit": self.unit,
+        }
+
 class By_Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    type_id = db.Column(db.Integer, db.ForeignKey('type.id'))
+    unit_id = db.Column(db.Integer, db.ForeignKey('unit.id'))
     name = db.Column(db.String(120), nullable=False)
-    price = db.Column(db.String(120), unique=True, nullable=False)
-    locationX = db.Column(db.Numeric(120), nullable=False)
-    locationY = db.Column(db.Numeric(120), nullable=False)
-    type = db.Column(db.String(120), nullable=False)
-    description = db.Column(db.String(120), nullable=False)
+    stock = db.Column(db.String(120))
+    price = db.Column(db.Numeric(120))
+    locationX = db.Column(db.Numeric(120))
+    locationY = db.Column(db.Numeric(120))
+    description = db.Column(db.String(120))
 
     def __repr__(self):
         return f'<By_Product {self.name}>'
@@ -44,20 +72,23 @@ class By_Product(db.Model):
         return {
             "id": self.id,
             "user_id": self.user_id,
+            "type_id": self.type_id,
+            "unit_id": self.unit_id,
             "name": self.name,
+            "stock": self.stock,
             "price": self.price,
-            "phone": self.phone,
             "locationX": self.locationX,
             "locationY":self.locationY,
-            "type": self.type,
             "description":self.description,
         }
 
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    img = db.Column(db.Text, unique=True, nullable=False)
+    mimetype = db.Column(db.Text, nullable=False)
+    by_product_id = db.Column(db.Integer, db.ForeignKey('by_product.id'))
     name = db.Column(db.String(120), nullable=False)
-    is_default = db.Column(db.Boolean(),)
+    is_default = db.Column(db.Boolean(), nullable=False)
 
     def __repr__(self):
         return f'<Image {self.name}>'
@@ -65,7 +96,102 @@ class Image(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "product_id": self.product_id,
+            "img": self.img,
+            "mimetype": self.mimetype,
+            "by_product_id": self.by_product_id,
             "name": self.name,
             "is_default": self.is_default
+        }
+
+class Status(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    usable_by_buyer = db.Column(db.Boolean())
+    usable_by_source = db.Column(db.Boolean())
+
+    def __repr__(self):
+        return f'<Status {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "usable_by_buyer": self.usable_by_buyer,
+            "usable_by_source": self.usable_by_source
+        }
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    status_id = db.Column(db.Integer, db.ForeignKey('status.id'))
+    total = db.Column(db.Numeric(120))
+    address = db.Column(db.String(120))
+    location = db.Column(db.String(120))
+
+        def __repr__(self):
+        return f'<Order {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "created_at": self.created_at,
+            "status_id": self.status_id,
+            "total": self.total,
+            "address": self.address,
+            "location": self.location
+        }
+
+class Order_Row(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    by_product_id = db.Column(db.Integer, db.ForeignKey('by_product.id'))
+    quantity = db.Column(db.Numeric(120))
+    subtotal = db.Column(db.Numeric(120))
+
+    def __repr__(self):
+        return f'<Order_Row {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "by_product_id": self.by_product_id,
+            "quantity": self.quantity,
+            "subtotal": self.subtotal
+        }
+    
+class Basket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    total = db.Column(db.Numeric(120))
+
+    def __repr__(self):
+        return f'<Basket {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "total": self.total
+        }
+
+class Basket_Row(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    basket_id = db.Column(db.Integer, db.ForeignKey('basket.id'))
+    by_product_id = db.Column(db.Integer, db.ForeignKey('by_product.id'))
+    quantity = db.Column(db.Numeric(120))
+    subtotal = db.Column(db.Numeric(120))
+
+    def __repr__(self):
+        return f'<Basket_Row {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "by_product_id": self.by_product_id,
+            "quantity": self.quantity,
+            "subtotal": self.subtotal
         }
