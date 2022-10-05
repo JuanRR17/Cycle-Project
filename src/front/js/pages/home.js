0c00d8a5bda640ed78f6b011822f3dd8edac5028
JuanRR17 <juanrepeto@gmail.com>
2022-10-05 09:58:56 +0000

    handling input errors

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,25 +1,33 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
+  const navigate = useNavigate();
 
   return (
     <div className="text-center mt-5">
-      <div>
-        <Link to="signup">Sign Up</Link>
-      </div>
-      <div>
-        <Link to="login">Login</Link>
-      </div>
-      <div>
-        <Link to="profile">Profile</Link>
-      </div>
-      <button onClick={() => actions.logout()} className="btn btn-danger">
-        Log out
-      </button>
+      {store.token ? (
+        <>
+          <div>
+            <Link to="profile">Profile</Link>
+          </div>
+          <button onClick={() => actions.logout()} className="btn btn-danger">
+            Log out
+          </button>
+        </>
+      ) : (
+        <>
+          <div>
+            <Link to="signup">Sign Up</Link>
+          </div>
+          <div>
+            <Link to="login">Login</Link>
+          </div>
+        </>
+      )}
     </div>
   );
 };
