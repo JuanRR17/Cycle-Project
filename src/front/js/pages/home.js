e95bb6e4edc83a1d990fe856e2e882c20cb457f4
anammunoz <anamariamunozcarcel@gmail.com>
2022-10-08 11:31:05 +0000

    navbar

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,15 +1,33 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
+import UserByProductsTable from "../component/user_profile/UserByProductsTable";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h2>What is Thinkay?</h2>
-			<p></p>
-		</div>
-	);
+  return (
+    <div className="text-left mt-5">
+      {store.token ? (
+        <>
+          <div>
+            <Link to="profile">Profile</Link>
+          </div>
+          <button onClick={() => actions.logout()} className="btn btn-danger">
+            Log out
+          </button>
+        </>
+      ) : (
+        <>
+          <div>
+            <Link to="signup">Sign Up</Link>
+          </div>
+          <div>
+            <Link to="login">Login</Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
