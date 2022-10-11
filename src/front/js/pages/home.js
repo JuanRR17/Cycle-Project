ae22a0034e8421815b952087461a1de5f299790d
JuanRR17 <juanrepeto@gmail.com>
2022-10-11 18:26:12 +0000

    navbar and home updated

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,35 +1,7 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
+import React from "react";
 import "../../styles/home.css";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
-  const { store, actions } = useContext(Context);
-
-  return (
-    <div className="text-left mt-5">
-      <div>
-        <Link to="prod_list">Products List</Link>
-      </div>
-      {store.token ? (
-        <>
-          <div>
-            <Link to="profile">Profile</Link>
-          </div>
-          <button onClick={() => actions.logout()} className="btn btn-danger">
-            Log out
-          </button>
-        </>
-      ) : (
-        <>
-          <div>
-            <Link to="signup">Sign Up</Link>
-          </div>
-          <div>
-            <Link to="login">Login</Link>
-          </div>
-        </>
-      )}
-    </div>
-  );
+  return <div className="text-center mt-5">Home</div>;
 };
