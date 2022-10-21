788a10e8b82f847145cf48d959927bb648bc8c40
JuanRR17 <juanrepeto@gmail.com>
2022-10-21 11:40:59 +0000

    navbar collapse sorted 2

--- src/front/js/component/Navbar.js
+++ src/front/js/component/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 text-center">
             <Link className="nav-item nav-link" to="/">
               <span>Home</span>
             </Link>
