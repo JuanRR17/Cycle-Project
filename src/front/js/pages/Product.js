175ed9d2655d4fa9f4d6df098186a82bccea179a
JuanRR17 <juanrepeto@gmail.com>
2022-10-25 18:26:40 +0000

    description

--- src/front/js/pages/Product.js
+++ src/front/js/pages/Product.js
@@ -63,51 +63,45 @@ export const Product = () => {
   return (
     <div className="mt-3">
       <div className="m-auto w-75  bg-warning p-5">
-        <button
-          type="button"
-          className="btn btn-danger btn-custom m-2 float-end"
-          onClick={() => navigate("/prod_list")}
-        >
-          <TiArrowBackOutline /> Back
-        </button>
         {store.product ? (
           <>
             <h1 className="text-center fw-bolder mb-4">{product.name}</h1>
             <div className="container-fluid mx-0 text-success">
               <div className="row gap-3">
-                <div className="col-lg-8">
+                <div className="col-lg-7">
                   <img
                     src={thinkay}
                     alt={product.name}
                     className="img-fluid img-thumbnail shadow"
                   />
                 </div>
-                <div className="row col-lg-4">
-                  <div className="col-4 col-lg-12">
+                <div className="row col-lg-5">
+                  <div className="col-sm-6 col-md-4 col-lg-12">
                     <label>Type:</label> {product.type}
                   </div>
                   {store.data ? (
                     <>
-                      <div className="col-4 col-lg-12">
+                      <div className="col-sm-6 col-md-4 col-lg-12">
                         {" "}
                         <label>Location:</label> {product.location}
                       </div>
                       {store.token ? (
-                        <div className="col-4 col-lg-12">
-                          <label>Price:</label>
-                          {product.price} €/{product.unit}
+                        <div className="col-sm-6 col-md-4 col-lg-12">
+                          <label>Price:</label> {product.price} €/{product.unit}
                         </div>
                       ) : (
                         ""
                       )}
-                      <div className="col-4 col-lg-12">
+                      <div className="col-sm-6 col-md-4 col-lg-12">
                         <label>Stock:</label> {product.stock} {product.unit}
                       </div>
                       {product.user_id === store.data.id ? (
-                        <div className="col-4 col-lg-12">Created By You</div>
+                        <div className="col-sm-6 col-md-4 col-lg-12">
+                          Created By You
+                        </div>
                       ) : (
                         <>
-                          <div className="col-4 col-lg-12">
+                          <div className="col-sm-6 col-md-4 col-lg-12">
                             {" "}
                             <label>Created By:</label> {product.user.username}
                           </div>
@@ -115,59 +109,81 @@ export const Product = () => {
                         <div>Email: {product.user.email}</div> */}
                         </>
                       )}
-                      <div className="col-lg-12">
-                        <label>Description:</label>
-                      </div>
-                      <div>{product.description}</div>
+
                       {store.token && product.user_id !== store.data.id ? (
                         <>
-                          <div className="col-4 col-lg-12">
+                          <div className="col-sm-6 col-md-4 col-lg-12">
                             <label>Quantity:</label>
-                          </div>
-                          <div className="col-4 col-lg-12">
-                            <Quantity
-                              quantity={quantity}
-                              stock={product.stock}
-                              handleSetQuantity={(value) => setQuantity(value)}
-                            />
-                            <span className="ms-1">{product.unit}</span>
+
+                            <div className="col-sm-6 col-md-4 col-lg-12">
+                              <Quantity
+                                quantity={quantity}
+                                stock={product.stock}
+                                handleSetQuantity={(value) =>
+                                  setQuantity(value)
+                                }
+                              />
+                              <span className="ms-1">{product.unit}</span>
+                            </div>
                           </div>
                         </>
                       ) : null}
                     </>
                   ) : null}
 
-                  <div className="d-flex gap-3 mt-2">
+                  <div className="d-flex gap-3 my-2">
                     {store.token && product.user_id !== store.data?.id ? (
                       <div>
                         <button
                           type="button"
-                          className="btn btn-success lh-sm px-4 py-2 btn-custom"
+                          className="btn btn-success lh-lg px-4 py-2 btn-custom"
                           onClick={handleBuy}
                         >
                           Buy
                         </button>
                       </div>
                     ) : null}
-                    <span>
+                    <div className="d-flex gap-3">
                       {store.message ? store.message : ""}
-                      <IconContext.Provider
-                        value={{ className: "mx-2", size: 30 }}
-                      >
+                      <IconContext.Provider value={{ className: "", size: 40 }}>
                         <>
                           <FavouriteIcon product={product} />
                           <BasketIcon product={product} />
                         </>
                       </IconContext.Provider>
-                    </span>
+                    </div>
                   </div>
+                </div>
+
+                <div className="col-lg-12">
+                  <label>Description:</label>
+                </div>
+                <div className="col-lg-12">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum.
+                  {product.description}
                 </div>
               </div>
             </div>
           </>
         ) : (
-          "This product doesn't exist"
+          <span className="text-danger">This product doesn't exist</span>
         )}
+        <button
+          type="button"
+          className="btn btn-danger btn-custom m-2 me-auto"
+          onClick={() => navigate("/prod_list")}
+        >
+          <TiArrowBackOutline /> Back
+        </button>
       </div>
     </div>
   );
