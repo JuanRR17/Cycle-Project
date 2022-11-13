9ffb70801e67f69160c532be14c95a31ccefe0c9
JuanRR17 <juanrepeto@gmail.com>
2022-11-13 10:11:07 +0000

    login

--- src/front/js/pages/Login.js
+++ src/front/js/pages/Login.js
@@ -43,53 +43,55 @@ const Login = () => {
 
   return (
     <div className="m-auto bg-custom">
-      <h1 className=" text-center">Login</h1>
+      <div className="login">
+        <h1 className=" text-center">Login</h1>
 
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className=" form-label">
-          Email address
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        {errors?.email ? (
-          <div className="text-error">{errors?.email}</div>
-        ) : null}
-        {store.message && store.message.split(" ")[1] === "email" ? (
-          <div className="text-error">{store.message}</div>
-        ) : null}
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className=" form-label">
-          Password
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {errors?.password ? (
-          <div className="text-error">{errors?.password}</div>
-        ) : null}
-        {store.message && (
-          <div className="text-error mb-3">{store.message}</div>
-        )}
-      </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className=" form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {errors?.email ? (
+            <div className="text-error">{errors?.email}</div>
+          ) : null}
+          {store.message && store.message.split(" ")[1] === "email" ? (
+            <div className="text-error">{store.message}</div>
+          ) : null}
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className=" form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {errors?.password ? (
+            <div className="text-error">{errors?.password}</div>
+          ) : null}
+          {store.message && (
+            <div className="text-error mb-3">{store.message}</div>
+          )}
+        </div>
 
-      <div className="py-2 d-flex gap-2">
-        <button className="btn btn-success btn-custom" onClick={handleLogin}>
-          Login
-        </button>
-        <button onClick={handleCancel} className="btn btn-danger btn-custom ">
-          Cancel
-        </button>
+        <div className="py-2 d-flex gap-2">
+          <button className="btn btn-success btn-custom" onClick={handleLogin}>
+            Login
+          </button>
+          <button onClick={handleCancel} className="btn btn-danger btn-custom ">
+            Cancel
+          </button>
+        </div>
       </div>
     </div>
   );
